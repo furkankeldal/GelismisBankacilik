5061c2b0411103d4cf12ebd66510fa9ffb4e6764
furkankeldal <keldal678@gmail.com>
2025-12-18 09:00:29 +0300

    pipeline test

--- microservices/account-service/src/test/java/com/example/OnlineBankacilik/integration/AccountServiceIntegrationTest.java
+++ microservices/account-service/src/test/java/com/example/OnlineBankacilik/integration/AccountServiceIntegrationTest.java
@@ -33,7 +33,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
-@ActiveProfiles("test")
+@ActiveProfiles("integration")
 @DisplayName("Account Service Full Integration Test")
 class AccountServiceIntegrationTest {
 
